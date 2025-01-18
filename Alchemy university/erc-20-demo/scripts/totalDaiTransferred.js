438923faf26946d6bcd5ccdbbb2b8dd7c7196e32
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2025-01-18 13:00:11 +0100

    add totalDaiTransferred func

---
+++ Alchemy university/erc-20-demo/scripts/totalDaiTransferred.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const { Alchemy, Network } = require("alchemy-sdk");
+const { firstTopic, secondTopic } = require('./topics');
+// prefix both the topics with 0x
+const topics = [firstTopic(), secondTopic()].map((x) => '0x' + x);
+
+const config = {
+    apiKey: process.env.API_KEY,
+    network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(config);
+
+async function totalDaiTransferred(fromBlock, toBlock) {
+    const logs = await alchemy.core.getLogs({
+        address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+        fromBlock,
+        toBlock,
+        topics
+    });
+
+
+    return logs.reduce((acc,log)=>acc+BigInt(log.data),BigInt(0));
+}
+
+module.exports = totalDaiTransferred;
